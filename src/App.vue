34d6cc73845e542d951c45c895a1217312a491b2
Martin Luberg <martin.luberg@tptlive.ee>
2022-09-27 10:15:01 +0300

    made some big changes to index.js, app.vue,ToDoList.vue

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,9 @@
 <template>
-<div class="container">
-    <section class="section">
-            <button class="button" @click="count++">{{ count }}</button>
-        <div class="field has-addons">
-            <p class="control">
-                <input type="text" v-model="newItem" @keydown.enter="addItem()">
-            </p>
-            <p class="control">
-                <button class="button" @click="addItem()">Add</button>
-            </p>
-        </div>
-    <div class="content">
-            <item-list title="All Items" v-bind:items="items"></item-list>
-            <item-list title="Done Items" v-bind:items="items"></item-list>
-            <item-list title="Not Done Items" v-bind:items="items"></item-list>
-        </div>
-  </section>
-</div>
+  <router-view></router-view>
 </template>
 
 <script>
-import ItemList from './ItemList.vue';
 export default {
-  components: { ItemList },
-    data(){
-        return{
-            name:"Martin Luberg",
-            count: 0,
-            items: [
-                {name:"sai",done: true, id: 0},
-                {name:"kali",done: true, id: 1},
-                {name:"sool",done: false, id: 2},
-                {name:"piim",done: true, id: 3},
-                {name:"leib",done: false, id: 4},
-                ]
-        }
-    },
-    methods: {
-        addItem(){
-            if(this.newItem.trim() != ''){
-                this.items.push({name: this.newItem,done: false, id: this.items.lenght});
-            }
-            this.newItem = '';
-        }
-    },
-    computed: { // it turnes mongus to bruh
-        doneItems(){
-            return this.items.filter(item => item.done)
-        },
-        notDoneItems(){
-            return this.items.filter(item => !item.done)
-        }
-    },
 
 }
 </script>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,22 @@ import './style.scss';
 
 import { createApp } from 'vue';
 
-import App from './App.vue';
+import * as VueRouter from 'vue-router';
 
-createApp(App).mount('#app');
+
+import App from './App.vue';
+import ToDoList from './ToDoList.vue';
+
+const routes = [
+    { path: '/', component: ToDoList },
+  ]
+
+  const router = VueRouter.createRouter({
+    history: VueRouter.createWebHashHistory(),
+    routes,
+  })
+
+let app = createApp(App)
+app.use(router);
+
+app.mount('#app');
